2c6ac727370aa4214ae88c637cdf6df30ee94d73
Tarik <testfromtaras@gmail.com>
2022-04-25 21:16:50 +0300

    File: config.py added

---
+++ utilities/config.py
@@ -0,0 +1,17 @@
+
+class Config:
+    pass
+    """"
+    def __init__(self, env):
+
+        self.chrome_driver = {
+            'windows': 'C:\\ChromeDriver\\chromedriver.exe',
+            'mac': '/Users/tar/Drivers/chromedriver'
+        }[env]
+
+        self.firefox_driver = {
+            'windows': 'C:\\firefoxDriver\\geckodriver.exe',
+            'mac': '/Users/tar/Drivers/geckodriver'
+        }[env]
+
+"""
